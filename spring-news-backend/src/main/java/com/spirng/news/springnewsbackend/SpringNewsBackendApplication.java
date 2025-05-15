b3e2a3665271e937ca472df1dd15d2c3532c4c8a
Mohamed Tahiri <medtohairi@gmail.com>
2025-05-15 19:51:37 +0100

    add project files

---
+++ spring-news-backend/src/main/java/com/spirng/news/springnewsbackend/SpringNewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spirng.news.springnewsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNewsBackendApplication.class, args);
+	}
+
+}
