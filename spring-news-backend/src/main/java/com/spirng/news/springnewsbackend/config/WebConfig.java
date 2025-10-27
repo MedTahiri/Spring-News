4eb52390796cb4b36040b417b856c4c6496d4122
Mohamed Tahiri <medtohairi@gmail.com>
2025-10-27 22:17:24 +0100

    update config for deploy

--- spring-news-backend/src/main/java/com/spirng/news/springnewsbackend/config/WebConfig.java
+++ spring-news-backend/src/main/java/com/spirng/news/springnewsbackend/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.spirng.news.springnewsbackend.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,12 +16,21 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                Dotenv dotenv = Dotenv.configure().load();
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("http://localhost:3000") // React/Next.js dev origin
+                        .allowedOrigins(dotenv.get("ORIGIN_URL")) // React/Next.js dev origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env"));
+        return configurer;
+    }
+
 }
